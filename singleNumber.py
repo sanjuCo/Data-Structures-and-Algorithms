93a2880882235710208aead5ef158651a343008a
Isaiah Onando Mulang <mulang.onando@gmail.com>
2024-02-05 12:50:40 +0300

    Update singleNumber.py

--- singleNumber.py
+++ singleNumber.py
@@ -1,3 +1,14 @@
+""" 
+It is better to have a comment here mentioning the question (problem you are solb=ving and explaining your solution
+"""
+"""
+Please place comments about the running time of your solution
+"""
+
+"""
+WRONG SOLUTION, CAN YOU READ THE QUESTION AGAIN.
+"""
+
 def singleNumber(nums):
     
     number = 0
