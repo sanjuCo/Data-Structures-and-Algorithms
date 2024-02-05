fc4c1691885a8254f6cc4a90ac3274fcf34e414d
Isaiah Onando Mulang <mulang.onando@gmail.com>
2024-02-05 12:45:13 +0300

    Update lookForDuplicate.py

--- lookForDuplicate.py
+++ lookForDuplicate.py
@@ -1,3 +1,9 @@
+""" 
+It is better to have a comment here mentioning the question (problem you are solb=ving and explaining your solution
+"""
+"""
+Please place comments about the running time of your solution
+"""
 def Duplicate(nums):
     n = set()
 
@@ -8,4 +14,4 @@ def Duplicate(nums):
         else:
              n.add(num)
         
-    return False
+    return False # Looks like you are always retruning False in this function
