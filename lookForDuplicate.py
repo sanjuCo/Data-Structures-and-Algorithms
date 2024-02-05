4c99aca74dd448d1c44ccdfb2e2adb8c983c2ff1
sanjuCo <145989871+sanjuCo@users.noreply.github.com>
2024-02-05 18:00:12 +0300

    Update lookForDuplicate.py

--- lookForDuplicate.py
+++ lookForDuplicate.py
@@ -4,6 +4,9 @@ It is better to have a comment here mentioning the question (problem you are sol
 """
 Please place comments about the running time of your solution
 """
+"""
+The time complexity of this is O(n) where n is the number of elements in the array
+"""
 def Duplicate(nums):
     n = set()
 
@@ -15,3 +18,5 @@ def Duplicate(nums):
              n.add(num)
         
     return False # Looks like you are always retruning False in this function
+
+#The function will only return False after the for loop and no duplicate is found because the return False statement is indented to the scope of the function itself
